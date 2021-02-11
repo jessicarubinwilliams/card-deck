63a4c6112270b1c090158db86db2a2c464074ca3
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-11 13:38:20 -0800

    Create deckArray.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
     const suitsArray = ["Hearts", "Diamonds", "Clubs", "Spades"];
     const ranksArray = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+    let deckArray = [];
     suitsArray.forEach(function(suit) {
       ranksArray.forEach(function(rank){
         card = rank + " of " + suit;
-        $("#output").append("<li>" + card + "</li>");
+        deckArray.push(card);
       });
+    });
+    deckArray.forEach(function(card) {
+      $("#output").append("<li>" + card + "</li>");
     });
   });
 });
